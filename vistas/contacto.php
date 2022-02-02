2e7fd3f1c477faee0cf101aede68a068cdb67ac1
Dragonf-j <jmjejuan@gmail.com>
2022-02-02 12:20:52 +0100

    update 02/02/2022 contacto v0.2

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -61,9 +61,9 @@
                         </li>
                         <li class="nav-item">
                             <a href="contacto.php" class="nav-link">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
-</svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                                </svg>
                                 Contacto</a>
                         </li>
                         </li>
@@ -73,15 +73,22 @@
         </nav>
 
     </header>
-    <div>
-    <form action="" method="post">
-        <label for="correo">
-            Correo:
-            <input type="email" name="correo" id="correo">
-        </label>
-        
+    <div class="container bg-light d-flex flex-column justify-content-around ">
+        <form action="" method="post">
+            <label for="correo">
+                Correo:
+                <input type="email" name="correo" id="correo">
+            </label>
+            <label for="asunto">
+                Asunto:
+                <input type="text" name="asunto" id="asunto">
+            </label>
+            <label for="">
+                Mensaje:
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </label>
 
-    </form>
+        </form>
 
     </div>
 
