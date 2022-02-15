9758b945ef79a09b438c458de38b9dc3fb340766
Dragonf-j <jmjejuan@gmail.com>
2022-02-15 12:07:38 +0100

    update 15/02/2022 v0.2

--- vistas/Vistas_dinamicas/montarTabla.php
+++ vistas/Vistas_dinamicas/montarTabla.php
@@ -50,71 +50,31 @@ class montarTabla
 
     public static function montar($datos)
     {
-if($datos!=0){
-        foreach ($datos as $key => $value) {
-            $nombre = $value['nombre'];
-            $img = $value["imagen"];
-            $tipo = $value['tipo'];
-            $ram = $value['ram'];
-            $enlaceA = $value['enlace_amazon'];
-            $precioa = $value['precio_amazon'];
-            //se rompen los enlaces de pc componentes
-            // $enlacePc = $value['enlace_pccomponetes'];
-            // $preciop = $value['precio_pcpcomponentes'];
-            $almacenamiento = $value['almacenamiento'];
-            $desc = $value['Descripcion'];
-        }
+        if($datos!=0){
+            foreach ($datos as $key => $value) {
+           
+                $enlace = $value['enlace'];
+            }
         
 
         ?>
             <div class="contenedor">
-                <h3 class="container bg-light p-2 ms-auto h3"><?php echo $nombre; ?></h3>
                 <?php
-                ?> <h4>TIpo: <?php echo $tipo; ?></h4>
-                <h6>Precio: <?php echo $precioa; ?>€</h6>
-                <?php
-                ?> <p>Memoria RAM: <?php echo $ram; ?>GB</p>
-                <?php
+                    echo $enlace;
                 ?>
-                <p>Almacenamiento en disco: <?php echo $almacenamiento; ?></p>
-                <img class="img-responsive img-thumbnail est" src="../img/<?php echo $img; ?>" alt="img">
-                <?php echo '<br>'; ?>
-                <button class="btn btn-outline-secondary">
-                    <a class="nav-link" id="enlace"   href="<?php echo $enlaceA; ?>" onclick="arbir()">Amazon</a>
-
-                </button>
-                <h6>Descripción</h6>
-                <p> <?php echo $desc; ?></p>
             </div>
 
-<?php
+    <?php
         }else{
-                echo "NO hay equipos con esas caracteristicas";
+                echo "No hay equipos con esas caracteristicas";
         }
     }
-    public static function montar2($datos)
-    {
-
-        foreach ($datos as $key => $value) {
-            $nemail = $value['email_user'];
-            $his = $value["historico"];
-
-            ?>
-            <div class="contenedor">
-                <!-- <h3 class="container bg-light p-2 ms-auto h3">Usuario<?php echo $nemail; ?></h3> -->
+    
                
-                
 
-            </div>
-            
-                        <?php  echo $his;?>
-               
-<?php
-           
-        }
-    }
+       
 }
-?>
+    ?>
 <style>
     .est {
         align-items: center;
