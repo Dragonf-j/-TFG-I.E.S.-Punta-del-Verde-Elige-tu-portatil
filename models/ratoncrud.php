be0ae6b2494a9fbc42465899112a4ee526a97585
Dragonf-j <jmjejuan@gmail.com>
2022-04-08 17:49:50 +0200

    update 08/04/2022 v0.2

--- models/ratoncrud.php
+++ models/ratoncrud.php
@@ -1,3 +1,13 @@
 <?php
+class ratoncrud{
+    private $conexion;
+    private $host;
+    private $users;
+    private $password;
+    private $consulta;
+    private $ratones;
+    private $nameDB;
 
+
+}
 ?>
